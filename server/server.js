cc1d0639d60faeeda5d0822dd2f7b247d85d5ecc
Benjamin Landry <benjamin.landry@ericsson.com>
2017-12-15 15:39:10 -0500

    socket connection working

--- server/server.js
+++ server/server.js
@@ -2,16 +2,17 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-var server = http.createServer(app);
+var server = http.createServer(app);  //step0 for opening socket 
 var io = socketIO(server);
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath));  //step1
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => { //step3
   console.log('New user connected');
 
   socket.on('disconnect', () => {
@@ -19,6 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.listen(port , () => {
+server.listen(port , () => {  //step2
   console.log(`Server is up on port ${port}`);
 });
