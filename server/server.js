8480a310c3bb27c052b6243358df74bb5659c875
Benjamin Landry <benjamin.landry@ericsson.com>
2017-12-18 14:05:47 -0500

    socket emit & on working

--- server/server.js
+++ server/server.js
@@ -2,24 +2,34 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const cors = require('cors');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 var app = express();
-var server = http.createServer(app);  //step0 for opening socket 
+var server = http.createServer(app);  //step0 for opening socket
 var io = socketIO(server);
 
-app.use(express.static(publicPath));  //step1
+app.use(express.static(publicPath));  //step1: use express middleware to grab file specified.
 
-io.on('connection', (socket) => { //step3
+io.on('connection', function (socket) { //step3
   console.log('New user connected');
+
+  socket.emit('newEmail', { //send data to socket.on
+    from: 'mike@example.com',
+    text: 'Hey. Hows it going?',
+    createdAt: 123
+  });
+
+  socket.on('createEmail', function (newEmail) { //receive data from 'socket.emit('<name>')'
+    console.log('createEmail', newEmail);
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
 
-server.listen(port , () => {  //step2
+server.listen(port , () => {  //step2: server listens on port#
   console.log(`Server is up on port ${port}`);
 });
