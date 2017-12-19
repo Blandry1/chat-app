ff9396f87063fa48462229a1fac042a81338f79e
Benjamin Landry <benjamin.landry@ericsson.com>
2017-12-19 15:20:03 -0500

    added jquery & msg form

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,34 @@ var socket = io();
 
 socket.on('connect', function ()  {
   console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
 
   socket.on('newMessage', function (message) {
     console.log('New message received', message )
-  });
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
 
-  socket.on('disconnect', function () {
-    console.log('Disconnected from server');
+    jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, function (data) {
+//   console.log('Got it!', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
   });
 });
